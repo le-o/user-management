646bf286fe00e34f5cced6a4fdf55dc518c5d608
leonard <leo.decaillet@d-web.ch>
2021-12-06 19:57:41 +0100

    Improvement #3 Add name and surname

--- views/user/_form.php
+++ views/user/_form.php
@@ -13,61 +13,65 @@ use webvimark\extensions\BootstrapSwitch\BootstrapSwitch;
  */
 ?>
 
-<div class="user-form">
+    <div class="user-form">
 
-	<?php $form = ActiveForm::begin([
-		'id'=>'user',
-		'layout'=>'horizontal',
-		'validateOnBlur' => false,
-	]); ?>
+        <?php $form = ActiveForm::begin([
+            'id'=>'user',
+            'layout'=>'horizontal',
+            'validateOnBlur' => false,
+        ]); ?>
 
-	<?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(User::getStatusList()) ?>
+        <?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(User::getStatusList()) ?>
 
-    <?= $form->field($model, 'qualification')->dropDownList((new \app\models\PowParameters)->optsQualification(), ['prompt'=>Yii::t('app', 'Choose a qualification')]) ?>
+        <?= $form->field($model, 'qualification')->dropDownList((new \app\models\PowParameters)->optsQualification(), ['prompt'=>Yii::t('app', 'Choose a qualification')]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+        <?= $form->field($model, 'surname')->textInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
 
-	<?php if ( $model->isNewRecord ): ?>
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
 
-		<?= $form->field($model, 'password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+        <?= $form->field($model, 'email')->textInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
 
-		<?= $form->field($model, 'repeat_password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
-	<?php endif; ?>
+        <?php if ( $model->isNewRecord ): ?>
 
+            <?= $form->field($model, 'password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
 
-	<?php if ( User::hasPermission('bindUserToIp') ): ?>
-
-		<?= $form->field($model, 'bind_to_ip')
-			->textInput(['maxlength' => 255])
-			->hint(UserManagementModule::t('back','For example: 123.34.56.78, 168.111.192.12')) ?>
-
-	<?php endif; ?>
-
-	<?php if ( User::hasPermission('editUserEmail') ): ?>
-
-		<?= $form->field($model, 'email_confirmed')->checkbox() ?>
-
-	<?php endif; ?>
+            <?= $form->field($model, 'repeat_password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+        <?php endif; ?>
 
 
-	<div class="form-group">
-		<div class="col-sm-offset-3 col-sm-9">
-			<?php if ( $model->isNewRecord ): ?>
-				<?= Html::submitButton(
-					'<span class="glyphicon glyphicon-plus-sign"></span> ' . UserManagementModule::t('back', 'Create'),
-					['class' => 'btn btn-success']
-				) ?>
-			<?php else: ?>
-				<?= Html::submitButton(
-					'<span class="glyphicon glyphicon-ok"></span> ' . UserManagementModule::t('back', 'Save'),
-					['class' => 'btn btn-primary']
-				) ?>
-			<?php endif; ?>
-		</div>
-	</div>
+        <?php if ( User::hasPermission('bindUserToIp') ): ?>
 
-	<?php ActiveForm::end(); ?>
+            <?= $form->field($model, 'bind_to_ip')
+                ->textInput(['maxlength' => 255])
+                ->hint(UserManagementModule::t('back','For example: 123.34.56.78, 168.111.192.12')) ?>
 
-</div>
+        <?php endif; ?>
+
+        <?php if ( User::hasPermission('editUserEmail') ): ?>
+
+            <?= $form->field($model, 'email_confirmed')->checkbox() ?>
+
+        <?php endif; ?>
+
+
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-9">
+                <?php if ( $model->isNewRecord ): ?>
+                    <?= Html::submitButton(
+                        '<span class="glyphicon glyphicon-plus-sign"></span> ' . UserManagementModule::t('back', 'Create'),
+                        ['class' => 'btn btn-success']
+                    ) ?>
+                <?php else: ?>
+                    <?= Html::submitButton(
+                        '<span class="glyphicon glyphicon-ok"></span> ' . UserManagementModule::t('back', 'Save'),
+                        ['class' => 'btn btn-primary']
+                    ) ?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
 
 <?php BootstrapSwitch::widget() ?>
