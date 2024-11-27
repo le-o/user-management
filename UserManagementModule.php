e1a9c1b516197eeee605e4f806fb1895d6de3bf0
leonard <leo.decaillet@d-web.ch>
2024-11-27 23:48:11 +0100

    main-vh Adaptation pour vertic-halle

--- UserManagementModule.php
+++ UserManagementModule.php
@@ -228,7 +228,7 @@ class UserManagementModule extends \yii\base\Module
 			Yii::$app->i18n->translations['modules/user-management/*'] = [
 				'class'          => 'yii\i18n\PhpMessageSource',
 				'sourceLanguage' => 'en',
-				'basePath'       => '@vendor/leo/module-user-management/messages',
+				'basePath'       => '@vendor/le-o/module-user-management/messages',
 				'fileMap'        => [
 					'modules/user-management/back' => 'back.php',
 					'modules/user-management/front' => 'front.php',
